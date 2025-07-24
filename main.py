f0b03aa311c390571a09703a39b07f06c38170fe
Kotaz <semechkagent@gmail.com>
2025-07-24 14:32:06 +0300

    feat: добавлена поддержка тестового сервера и конфигурация верификации пользователей

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ TOKEN = os.getenv("TOKEN")
 if TOKEN is None:
     raise RuntimeError("Environment variable 'TOKEN' is not set. Please set it in your .env file or environment.")
 
-
-intents = disnake.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
-
 # ID тестового сервера
 GUILD_ID = int(os.getenv("GUILD_ID", -1))
 if GUILD_ID == -1:
     raise RuntimeError("Environment variable 'GUILD_ID' is not set. Please set it in your .env file or environment.")
 
+
+intents = disnake.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents, test_guilds=[GUILD_ID])
+
 # Загрузка экстеншенов
 bot.load_extension("extensions.verification")
 bot.load_extension("extensions.admin_roles")
 
+print(bot.extensions)
+
 @bot.slash_command(
     name="meow",
-    guild_ids=[GUILD_ID],
     description="Тестовая команда",
 )
 async def meow(inter: disnake.ApplicationCommandInteraction):
